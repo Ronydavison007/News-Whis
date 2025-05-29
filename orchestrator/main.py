3b737d274dee0f8cc93d3bb235ef428568d509a6
Ron Antony David <112165001+Ronydavison007@users.noreply.github.com>
2025-05-29 16:49:35 +0530

    Update main.py

--- orchestrator/main.py
+++ orchestrator/main.py
@@ -64,5 +64,3 @@ async def process_query(query: str = None, audio: UploadFile = File(None)):
                 except:
                     pass
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
